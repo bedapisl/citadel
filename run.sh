9d6b643a10bde9c7bc9e6babbfbc820bcb444d27
spekoun <beda@debian>
2015-02-17 20:50:11 +0100

    Working run script

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-export LD_LIBRARY_PATH="lib32:$LD_LIBRARY_PATH"
 
-exec "./citadel32.out" "$@"
+if test `uname -m` = "x86_64"
+then
+	BIT="64"
+else
+	BIT="32"
+fi
+
+export LD_LIBRARY_PATH="lib$BIT:$LD_LIBRARY_PATH"
+
+exec "./citadel$BIT.out" "$@"
 
 
 
